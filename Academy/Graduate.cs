66f3842b6405d59876ce50cfe5392768a7aebb48
Дарья Серова <dashaflay2000@gmail.com>
2025-11-05 11:48:26 +0300

    Add project files.

---
+++ Academy/Graduate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Graduate : Student
+	{
+		public string Subject { get; set; }
+
+		public Graduate
+			(
+			string lastName, string firstName, int age,
+			string speciality, string group, double rating, double attendance,string subject
+			) : base(lastName, firstName, age, speciality, group, rating, attendance)
+		{
+			Subject=subject;
+		}
+
+		public Graduate(Student student, string subject) : base(student)
+		{
+			Subject = subject;
+		}
+
+		~Graduate()
+		{
+			Console.WriteLine($"GDestructor:\t{GetHashCode()}");
+		}
+
+		public void Info()
+		{
+			base.Info();
+			Console.WriteLine(Subject);
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + Subject;
+		}
+
+	}
+}
---
+++ Academy/Teacher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Teacher : Human
+	{
+		public string Speciality { get; set; }
+		public int Experience { get; set; }
+
+		public Teacher
+			(
+			string lastName, string firstName, int age,
+            string speciality, int experience
+			):base (lastName, firstName, age)
+		{
+			Speciality = speciality;
+			Experience = experience;
+			Console.WriteLine($"TConstructor:\t{GetHashCode()}");
+		}
+
+		public Teacher(Human human, string speciality, int experience) : base(human)
+		{
+			Speciality=speciality;
+			Experience = experience;
+		}
+		~Teacher ()
+		{
+			Console.WriteLine($"TDestructor:\t{GetHashCode()}");
+		}
+
+		public void Info()
+		{
+			base.Info();
+			Console.WriteLine($"{Speciality} {Experience}");
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + $"{Speciality.PadRight(25)}{Experience}";
+		}
+
+	}
+}
